46f74b90fea2cdcd8ee03c8893775be99dd0909e
Salma El-adawi <113279700+20214447072salma@users.noreply.github.com>
2024-10-14 11:11:11 +0300

    Update gameJS.js

--- gameJS.js
+++ gameJS.js
@@ -29,7 +29,7 @@ let gameInterval, wallInterval;
 const images = new Image();
 images.src = 'images/robotLeft.png';
 images.src = 'images/robotRight.png';
-images.src = 'images/wall.png';
+images.src = 'wall.png';
 
 // Start game
 function startGame() {
