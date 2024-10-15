8ec438a5498fc06226b9881e14f16b221bd059b0
Salma El-adawi <113279700+20214447072salma@users.noreply.github.com>
2024-10-15 10:26:13 +0300

    Update gameJS.js

--- gameJS.js
+++ gameJS.js
@@ -172,7 +172,7 @@ function endGame() {
 setTimeout(endGame, 30000);
 
 function saveScore(score) {
-    fetch('http://localhost:8081/update_score', {
+    fetch('http://127.0.0.1:8081/update_score', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
