fd40cb055107c36a888159cdf64f9abd7cd20761
Salma El-adawi <113279700+20214447072salma@users.noreply.github.com>
2024-11-05 16:01:17 +0200

    Update home.js

--- home.js
+++ home.js
@@ -20,7 +20,7 @@ function updateTimer() {
     document.getElementById('timer').innerText = `${formatTime(timerhours)}:${formatTime(timerMinutes)}:${formatTime(timerSeconds)}`;
     
     // Decrement the time
-    if (timerSeconds > 1) {
+    if (timerSeconds > 0) {
         timerSeconds--;
     } else if (timerMinutes > 0) {
         timerMinutes--;
